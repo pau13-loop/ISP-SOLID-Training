15e016427c05e00704c6675511763c849247d9b8
pau <pllinas@cifpfbmoll.eu>
2021-03-10 11:55:05 +0100

    feat: :sparkles: Creation main app file

---
+++ isp/src/main/java/edu/pingpong/isp/main/ProcesadorTextoApp.java
@@ -0,0 +1,25 @@
+package edu.pingpong.isp.main;
+
+import edu.pingpong.isp.isp.ProcesadorTexto;
+
+public class ProcesadorTextoApp 
+{
+    public static void main( String[] args )
+    {
+
+        ProcesadorTexto procesador = new ProcesadorTexto();
+
+        // Adding the sentence
+        procesador.nueva("This");
+        procesador.nueva("is");
+        procesador.nueva("an");
+        procesador.nueva("ISP");
+        procesador.nueva("Java");
+        procesador.nueva("Kata");
+
+        // Printing the sentence we just added before
+        System.out.println("\n=================Trial Sentence=================\n");
+        System.out.println(procesador.texto());
+
+    }
+}
