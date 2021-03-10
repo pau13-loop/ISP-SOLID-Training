ac15cdd9323ba9fb3305d1ffc6b8a54f2df1ecf3
pau <pllinas@cifpfbmoll.eu>
2021-03-10 11:56:21 +0100

    feat: :sparkles: Creation idioma class

---
+++ isp/src/main/java/edu/pingpong/isp/isp/Idioma.java
@@ -0,0 +1,17 @@
+package edu.pingpong.isp.isp;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Idioma {
+	
+	EN ("I","am","angry"), 
+	ES ("tengo","hambre"), 
+	CA ("tenc", "fam");
+	
+	List<String> diccionario;
+
+	private Idioma(String ... palabras) {
+		this.diccionario = Arrays.asList(palabras);
+	}
+}
